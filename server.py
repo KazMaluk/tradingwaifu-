a69fac2e5a54f3cff12ae12e33fc991c949db1fb
KazMaluk <ashks.98899@gmail.com>
2025-03-03 21:54:02 -0500

    Update server.py

--- server.py
+++ server.py
@@ -2,9 +2,15 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__, static_folder="static")
 
+# Serve `index.html` at root `/`
 @app.route("/")
 def serve_home():
     return send_from_directory("static", "index.html")
+
+# Serve static files (CSS, JS, Images)
+@app.route("/static/<path:path>")
+def serve_static(path):
+    return send_from_directory("static", path)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
